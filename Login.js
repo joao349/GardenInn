7eed826df7d14933a39cafffa7b0c9e188b2cfd3
joao349 <j4389228@gmail.com>
2025-10-13 08:46:26 -0300

    porra

---
+++ Login.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoClient = require('mongodb').mongoClient;
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(session({
+    secret: 'Comi9Tijolos',
+    resave: false,
+    saveUnintialized : true,
+}));
+
+const urlMongo = 'mongodb://127.0.0.1:27017';
+const nomeBanco = 'sistemaLogin';
+
+app.get('registro', (req,res) => {
+    res.sendFile(__dirname + '/views/registro.html');
+});
+
+app.post('/registro', async (res,req)=>{
+    
+});
